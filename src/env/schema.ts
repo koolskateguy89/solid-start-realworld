24864953ae4907fedf63f552780e52af371ebc1b
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2023-02-01 08:14:57 +0000

    Cleanup

--- src/env/schema.ts
+++ src/env/schema.ts
@@ -1,16 +1,16 @@
 import { z } from "zod";
 
 export const serverScheme = z.object({
-  NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
-  ENABLE_VC_BUILD: z.string().default("1").transform((v) => parseInt(v)),
-  GITHUB_ID: z.string(),
-  GITHUB_SECRET: z.string(),
-  AUTH_SECRET: z.string(),
-  AUTH_TRUST_HOST: z.string().optional(),
-  NEXTAUTH_URL: z.string().optional(),
+  NODE_ENV: z
+    .enum(["development", "production", "test"])
+    .default("development"),
+  ENABLE_VC_BUILD: z
+    .string()
+    .default("1")
+    .transform((v) => parseInt(v)),
   DATABASE_URL: z.string(),
 });
 
 export const clientScheme = z.object({
-  MODE: z.enum(['development', 'production', 'test']).default('development'),
+  MODE: z.enum(["development", "production", "test"]).default("development"),
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import solid from "solid-start/vite";
 import { defineConfig } from "vite";
+import solid from "solid-start/vite";
 // @ts-expect-error no typing
 import vercel from "solid-start-vercel";
-  
+
 export default defineConfig(() => {
   return {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     plugins: [solid({ ssr: true, adapter: vercel({ edge: false }) })],
   };
 });
-  
