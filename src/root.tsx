12e6089b9598cc7f9b41a8c9bfc3c9d729855488
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2023-02-01 08:16:46 +0000

    Add stylesheet to root

--- src/root.tsx
+++ src/root.tsx
@@ -1,5 +1,4 @@
 // @refresh reload
-import "./root.css";
 import { Suspense } from "solid-js";
 import {
   Body,
@@ -11,15 +10,31 @@ import {
   Routes,
   Scripts,
   Title,
+  Link,
 } from "solid-start";
+
+// https://realworld-docs.netlify.app/docs/specs/frontend-specs/styles
 
 export default function Root() {
   return (
     <Html lang="en">
       <Head>
-        <Title>Create JD App</Title>
+        <Title>Conduit</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* Ionicon icons & Google Fonts the Bootstrap theme relies on */}
+        <Link
+          href="//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"
+          rel="stylesheet"
+          type="text/css"
+        />
+        <Link
+          href="//fonts.googleapis.com/css?family=Titillium+Web:700|Source+Serif+Pro:400,700|Merriweather+Sans:400,700|Source+Sans+Pro:400,300,600,700,300italic,400italic,600italic,700italic"
+          rel="stylesheet"
+          type="text/css"
+        />
+        {/* Custom Bootstrap 4 theme */}
+        <Link rel="stylesheet" href="//demo.productionready.io/main.css" />
       </Head>
       <Body>
         <Suspense>
