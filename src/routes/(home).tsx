89431c3219b1b2f62d3de88dfc27a5c87d1f5cff
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2023-02-03 00:09:15 +0000

    Use location instead of useSearchParams

--- src/routes/(home).tsx
+++ src/routes/(home).tsx
@@ -1,9 +1,9 @@
 import { type VoidComponent, createSignal, Show, Suspense } from "solid-js";
 import {
+  type RouteDataArgs,
   createRouteData,
   useRouteData,
   useSearchParams,
-  Head,
   Title,
   A,
 } from "solid-start";
@@ -13,23 +13,21 @@ import Banner from "~/components/home/Banner";
 import Sidebar from "~/components/home/Sidebar";
 import Articles from "~/components/article/Articles";
 
-export function routeData() {
-  const [searchParams] = useSearchParams<{ tag?: string }>();
-
+export function routeData({ location }: RouteDataArgs) {
   return createRouteData(
     async (tag, { fetch }) => {
-      // TODO: other search params? dont think so actually for this page
       const url = tag ? `/api/articles?tag=${tag}` : "/api/articles";
 
       const res = await fetch(url, {});
       const multipleArticles = (await res.json()) as MultipleArticles;
 
-      // TODO: feed global vs your, could use query param
+      // TODO: global feed vs your feed, could use query param, idrk
 
       return multipleArticles.articles;
     },
     {
-      key: () => searchParams.tag ?? "",
+      // using default "" because it won't run if tag is undefined
+      key: () => location.query.tag ?? "",
     }
   );
 }
@@ -48,12 +46,9 @@ const HomePage: VoidComponent = () => {
 
   return (
     <main class="home-page">
-      <Head>
-        <Title>Home — Conduit</Title>
-      </Head>
+      <Title>Home — Conduit</Title>
 
-      <pre>{JSON.stringify(searchParams)}</pre>
-
+      <pre>searchParams = {JSON.stringify(searchParams)}</pre>
       <Banner />
 
       <div class="container page">
@@ -98,62 +93,14 @@ const HomePage: VoidComponent = () => {
               </ul>
             </div>
 
-            <Suspense>
-              <Articles articles={articles() ?? []} />
+            <Suspense fallback="Loading articles...">
+              <Show
+                when={(articles() ?? []).length > 0}
+                fallback="Nothing to see here..."
+              >
+                <Articles articles={articles()!} />
+              </Show>
             </Suspense>
-
-            <div class="article-preview">
-              <div class="article-meta">
-                <A href="profile.html">
-                  <img
-                    src="http://i.imgur.com/Qr71crq.jpg"
-                    alt="idk i think user profile pic"
-                  />
-                </A>
-                <div class="info">
-                  <A href="" class="author">
-                    Eric Simons
-                  </A>
-                  <span class="date">January 20th</span>
-                </div>
-                <button class="btn btn-outline-primary btn-sm pull-xs-right">
-                  <i class="ion-heart" /> 29
-                </button>
-              </div>
-              <A href="" class="preview-link">
-                <h1>How to build webapps that scale</h1>
-                <p>This is the description for the post.</p>
-                <span>Read more...</span>
-              </A>
-            </div>
-
-            <div class="article-preview">
-              <div class="article-meta">
-                <A href="profile.html">
-                  <img
-                    src="http://i.imgur.com/N4VcUeJ.jpg"
-                    alt="idk i think author profile pic"
-                  />
-                </A>
-                <div class="info">
-                  <A href="" class="author">
-                    Albert Pai
-                  </A>
-                  <span class="date">January 20th</span>
-                </div>
-                <button class="btn btn-outline-primary btn-sm pull-xs-right">
-                  <i class="ion-heart" /> 32
-                </button>
-              </div>
-              <A href="" class="preview-link">
-                <h1>
-                  The song you won't ever stop singing. No matter how hard you
-                  try.
-                </h1>
-                <p>This is the description for the post.</p>
-                <span>Read more...</span>
-              </A>
-            </div>
           </div>
 
           <div class="col-md-3">
