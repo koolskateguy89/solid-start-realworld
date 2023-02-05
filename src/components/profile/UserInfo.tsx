504788a12bdb2fc03434fd4b21806ca47bdcbb26
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2023-02-05 23:20:05 +0000

    Add profile components

---
+++ src/components/profile/UserInfo.tsx
@@ -0,0 +1,42 @@
+import { type VoidComponent, Show } from "solid-js";
+
+import type { Profile } from "~/types/api";
+import { useSession } from "~/lib/session";
+
+export type UserInfoProps = Profile;
+
+const UserInfo: VoidComponent<UserInfoProps> = (props) => {
+  const session = useSession();
+
+  const canFollow = () => session()?.user?.username !== props.username;
+
+  return (
+    <div class="user-info">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12 col-md-10 offset-md-1">
+            <img src={props.image} alt={props.username} class="user-img" />
+            <h4>{props.username}</h4>
+            <p>{props.bio}</p>
+            <Show when={canFollow()}>
+              <button
+                type="button"
+                class="btn btn-sm action-btn"
+                classList={{
+                  "btn-secondary": props.following,
+                  "btn-outline-secondary": !props.following,
+                }}
+              >
+                <i class="ion-plus-round" />
+                &nbsp; {props.following ? "Unfollow" : "Follow"}{" "}
+                {props.username}
+              </button>
+            </Show>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserInfo;
