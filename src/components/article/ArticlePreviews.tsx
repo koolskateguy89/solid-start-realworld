fa32631a233179d75c4f0eb3309e44e9d7230279
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2023-02-05 17:19:39 +0000

    Rename Articles to ArticlePreviews

--- src/components/article/ArticlePreviews.tsx
+++ src/components/article/ArticlePreviews.tsx
@@ -3,11 +3,11 @@ import { type VoidComponent, For } from "solid-js";
 import type { Article } from "~/types/api";
 import ArticlePreview from "./ArticlePreview";
 
-export type ArticlesProps = {
+export type ArticlePreviewsProps = {
   articles: Article[];
 };
 
-const Articles: VoidComponent<ArticlesProps> = (props) => {
+const ArticlePreviews: VoidComponent<ArticlePreviewsProps> = (props) => {
   return (
     <For each={props.articles}>
       {(article) => <ArticlePreview {...article} />}
@@ -15,4 +15,4 @@ const Articles: VoidComponent<ArticlesProps> = (props) => {
   );
 };
 
-export default Articles;
+export default ArticlePreviews;
