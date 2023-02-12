b0db723bce9d4c84d8ddf19d2500c67af52c3b1a
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2023-02-12 03:10:25 +0000

    CommentForm -> NewCommentForm, implement adding comment

---
+++ src/components/comment/NewCommentForm.tsx
@@ -0,0 +1,91 @@
+import { type VoidComponent, Show } from "solid-js";
+import { useParams, A } from "solid-start";
+import { createServerAction$ } from "solid-start/server";
+
+import type { CreateCommentBody } from "~/routes/api/articles/[slug]/comments/(comment)";
+import type { Comment } from "~/types/api";
+import { useSession } from "~/lib/session";
+import ErrorsList from "~/components/user/ErrorsList";
+
+export type NewCommentFormProps = {
+  slug: string;
+};
+
+const NewCommentForm: VoidComponent<NewCommentFormProps> = () => {
+  const session = useSession();
+  const user = () => session()?.user;
+
+  const params = useParams<{ slug: string }>();
+
+  const [posting, { Form }] = createServerAction$(
+    async (formData: FormData, { fetch, request }) => {
+      const slug = formData.get("slug") as string;
+
+      const body: CreateCommentBody = {
+        comment: {
+          body: formData.get("body") as string,
+        },
+      };
+
+      const res = await fetch(`/api/articles/${slug}/comments`, {
+        method: "POST",
+        headers: request.headers,
+        body: JSON.stringify(body),
+      });
+
+      if (!res.ok) throw ["Invalid comment"];
+
+      // TODO: clear form on success, idk how tho
+
+      return (await res.json()) as Comment;
+    }
+  );
+
+  return (
+    <Show
+      when={user()}
+      fallback={
+        <p>
+          <A href="/login">Sign in</A> or <A href="/register">sign up</A> to add
+          comments on this article.
+        </p>
+      }
+      keyed
+    >
+      {(user) => (
+        <>
+          <ErrorsList errors={posting.error && posting.error} />
+
+          <Form class="card comment-form">
+            <input type="hidden" name="slug" value={params.slug} />
+            <div class="card-block">
+              <textarea
+                name="body"
+                class="form-control"
+                placeholder="Write a comment..."
+                rows="3"
+                required
+              />
+            </div>
+            <div class="card-footer">
+              <img
+                src={user.image}
+                alt={user.username}
+                class="comment-author-img"
+              />
+              <button
+                type="submit"
+                class="btn btn-sm btn-primary"
+                disabled={posting.pending}
+              >
+                Post Comment
+              </button>
+            </div>
+          </Form>
+        </>
+      )}
+    </Show>
+  );
+};
+
+export default NewCommentForm;
