5d979790b88a566bfda2dd740dc603d1fb0d99de
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2023-02-13 11:06:06 +0000

    Finish slug generator

--- src/server/lib/slug.ts
+++ src/server/lib/slug.ts
@@ -1,5 +1,4 @@
+// https://stackoverflow.com/a/1983661/17381629
 export function generateSlug(title: string) {
-  // TODO: need to replace group of spaces with dash
-  // this just replaces each space with dash
-  return title.toLowerCase().replaceAll(/ /g, "-");
+  return title.replace(/\s+/g, "-").toLowerCase();
 }
