3bf4e8ecee58368c2c5f68351128ca48893fe330
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2023-02-14 01:52:08 +0000

    Use nested route to define layout, not just data

--- src/routes/profile/[username].tsx
+++ src/routes/profile/[username].tsx
@@ -1,10 +1,20 @@
-import { type RouteDataArgs, redirect, Outlet } from "solid-start";
+import { Show } from "solid-js";
+import type { VoidComponent } from "solid-js";
+import {
+  type RouteDataArgs,
+  redirect,
+  useRouteData,
+  Title,
+  A,
+  Outlet,
+} from "solid-start";
 import { createServerData$ } from "solid-start/server";
 
 import type { Profile } from "~/types/api";
+import UserInfo from "~/components/profile/UserInfo";
 
 export function routeData({ params }: RouteDataArgs) {
-  const profile = createServerData$(
+  return createServerData$(
     async (username, { fetch, request }) => {
       const res = await fetch(`/api/profiles/${encodeURIComponent(username)}`, {
         headers: request.headers,
@@ -18,8 +28,52 @@ export function routeData({ params }: RouteDataArgs) {
       key: () => params.username,
     }
   );
-
-  return { profile };
 }
 
-export default Outlet;
+const ProfilePageLayout: VoidComponent = () => {
+  const profile = useRouteData<typeof routeData>();
+
+  return (
+    <Show when={profile()} keyed>
+      {(profile) => (
+        <main class="profile-page">
+          <Title>{profile.username} — Conduit</Title>
+
+          <UserInfo {...profile} />
+
+          <div class="container">
+            <div class="row">
+              <div class="col-xs-12 col-md-10 offset-md-1">
+                <div class="articles-toggle">
+                  <ul class="nav nav-pills outline-active">
+                    <li class="nav-item">
+                      <A
+                        href={`/profile/${profile.username}`}
+                        class="nav-link"
+                        end
+                      >
+                        My Articles
+                      </A>
+                    </li>
+                    <li class="nav-item">
+                      <A
+                        href={`/profile/${profile.username}/favorites`}
+                        class="nav-link"
+                      >
+                        Favorited Articles
+                      </A>
+                    </li>
+                  </ul>
+                </div>
+
+                <Outlet />
+              </div>
+            </div>
+          </div>
+        </main>
+      )}
+    </Show>
+  );
+};
+
+export default ProfilePageLayout;
--- src/routes/profile/[username]/favorites.tsx
+++ src/routes/profile/[username]/favorites.tsx
@@ -4,21 +4,14 @@ import {
   redirect,
   useRouteData,
   useParams,
-  Title,
-  A,
 } from "solid-start";
 import { createServerData$ } from "solid-start/server";
 
-import type { routeData as profileRouteData } from "../[username]";
 import type { MultipleArticles } from "~/types/api";
 import ArticlePreview from "~/components/article/ArticlePreview";
-import UserInfo from "~/components/profile/UserInfo";
 
-export function routeData({
-  params,
-  data: { profile },
-}: RouteDataArgs<typeof profileRouteData>) {
-  const articles = createServerData$(
+export function routeData({ params }: RouteDataArgs) {
+  return createServerData$(
     async ([, username], { fetch, request }) => {
       const res = await fetch(
         `/api/articles?favorited=${encodeURIComponent(username)}`,
@@ -27,60 +20,31 @@ export function routeData({
 
       if (!res.ok) throw redirect("/");
 
-      const multipleArticles = (await res.json()) as MultipleArticles;
-
-      return multipleArticles.articles;
+      const { articles } = (await res.json()) as MultipleArticles;
+      return articles;
     },
     {
       key: () => ["articles", params.username!] as const,
     }
   );
-
-  return { profile, articles };
 }
 
-// TODO: use [username].tsx to define layout not just data
 const FavoritesPage: VoidComponent = () => {
-  const { profile, articles } = useRouteData<typeof routeData>();
+  const articles = useRouteData<typeof routeData>();
 
   const params = useParams<{ username: string }>();
 
   return (
-    <main class="profile-page">
-      <Title>{profile()?.username ?? "Profile"} — Conduit</Title>
-
-      <UserInfo {...profile()!} />
-
-      <div class="container">
-        <div class="row">
-          <div class="col-xs-12 col-md-10 offset-md-1">
-            <div class="articles-toggle">
-              <ul class="nav nav-pills outline-active">
-                <li class="nav-item">
-                  <A href=".." class="nav-link">
-                    My Articles
-                  </A>
-                </li>
-                <li class="nav-item">
-                  <div class="nav-link active">Favorited Articles</div>
-                </li>
-              </ul>
-            </div>
-
-            <Suspense fallback="Loading articles...">
-              <For each={articles()} fallback="Nothing to see here...">
-                {(article) => (
-                  <ArticlePreview
-                    {...article}
-                    invalidate={["articles", params.username]}
-                  />
-                )}
-              </For>
-            </Suspense>
-          </div>
-        </div>
-      </div>
-    </main>
+    <Suspense fallback="Loading articles...">
+      <For each={articles()} fallback="Nothing to see here...">
+        {(article) => (
+          <ArticlePreview
+            {...article}
+            invalidate={["articles", params.username]}
+          />
+        )}
+      </For>
+    </Suspense>
   );
 };
 
