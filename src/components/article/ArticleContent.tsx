b35b3b67643af226cb4d3db58989cafc05b609b5
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2023-05-02 19:17:32 +0100

    Upgrade dependencies and implement syntax highlighting with solid-highlight & highlight.js

--- src/components/article/ArticleContent.tsx
+++ src/components/article/ArticleContent.tsx
@@ -1,31 +1,39 @@
 import type { ComponentProps, JSX, FlowComponent, Component } from "solid-js";
 import { splitProps, Show } from "solid-js";
 import { clientOnly } from "solid-start/islands";
-// import type { SolidMarkdownProps } from "solid-markdown/dist/complex-types";
+
+import "highlight.js/styles/atom-one-dark.css";
 
 // SolidMarkdown doesn't work with SSR, see
 // https://github.com/andi23rosca/solid-markdown/issues/3
 const SolidMarkdown = clientOnly(() => import("solid-markdown"));
 
-// TODO: syntax highlighting, see
-// https://github.com/remarkjs/react-markdown#use-custom-components-syntax-highlight
-
-// declare const SyntaxHighlighter: (props: any) => JSX.Element;
-const SyntaxHighlighter: Component<any> = (props) => null;
+// https://github.com/aidanaden/solid-highlight
+// solid-highlight also seems to not work with SSR
+const Highlight = clientOnly(() => import("solid-highlight"));
 
 type SolidMarkdownProps = ComponentProps<typeof SolidMarkdown>;
 
-// SolidMarkdown doesn't export the types of the separate components :cry:
-type CodeComponentProps = ComponentProps<
+type SolidMarkdownComponents = NonNullable<SolidMarkdownProps["components"]>;
+
+// SolidMarkdown doesn't export the types of the separate components
+type SolidMarkdownComponentProps<
+  TagName extends keyof SolidMarkdownComponents
+> = ComponentProps<
   Exclude<
-    NonNullable<SolidMarkdownProps["components"]>["code"],
+    SolidMarkdownComponents[TagName],
     keyof JSX.IntrinsicElements | undefined
   >
 >;
 
-// code for component pretty much taken directly from
-// https://github.com/remarkjs/react-markdown#use-custom-components-syntax-highlight
-// !!!! ffs SolidMarkdown uses className not class
+type CodeComponentProps = SolidMarkdownComponentProps<"code">;
+
+/**
+ * Syntax highlighting with solid-highlight for code blocks
+ *
+ * !!!! ffs SolidMarkdown uses className not class
+ * @see https://github.com/remarkjs/react-markdown#use-custom-components-syntax-highlight
+ */
 const Code: Component<CodeComponentProps & { className: string }> = (
   _props
 ) => {
@@ -34,20 +42,14 @@ const Code: Component<CodeComponentProps & { className: string }> = (
 
   const match = () => /language-(\w+)/.exec(props.className || "");
 
+  const children = () => String(props.children).replace(/\n$/, "");
+
   return (
     <Show when={!props.inline && match()} fallback={<code {...props} />} keyed>
       {(match) => (
-        <>
-          <SyntaxHighlighter
-            {...props}
-            children={String(props.children).replace(/\n$/, "")}
-            // import {dark} from 'react-syntax-highlighter/dist/esm/styles/prism'
-            // style={dark}
-            language={match[1]}
-            PreTag="div"
-          />
-          <code {...props}>hahahaah!</code>
-        </>
+        <Highlight {...props} language={match[1]} autoDetect={false}>
+          {children()}
+        </Highlight>
       )}
     </Show>
   );
@@ -58,7 +60,7 @@ const ArticleContent: FlowComponent<unknown, string> = (props) => (
     <article class="col-md-12">
       <SolidMarkdown
         components={{
-          // @ts-expect-error SolidMarkdown uses `className` not `class` (don't think it's ported from react properly)
+          // @ts-expect-error SolidMarkdown uses `className` not `class` (is it ported from react properly?)
           code: Code,
         }}
       >
